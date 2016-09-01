76cf56e20e97181b6d5ef70cbb89d06a96ba5435
Matthew Perkins <mfperkins@gmail.com>
2016-09-01 17:00:39 +0100

    27) refactoring

--- battle/lib/game.rb
+++ battle/lib/game.rb
@@ -2,21 +2,34 @@
 
 class Game
 
-  attr_reader :player_1, :player_2, :current_player
+  attr_reader :current_player, :players
 
   def initialize(player_1, player_2)
-    @player_1 = player_1
-    @player_2 = player_2
+    @players = [player_1, player_2]
     @current_player = player_1
   end
 
   def switch_turns
-    current_player == player_1 ? @current_player = player_2 : @current_player = player_1
+    @current_player = opponent_of(current_player)
+  end
+
+  def player_1
+    players.first
+  end
+
+  def player_2
+    players.last
   end
 
   def attack(player)
     player.receive_damage
     switch_turns
+  end
+
+  private
+
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 
 end
--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -7,12 +7,18 @@ feature 'Enter names' do
   end
 end
 
-feature 'See hit points' do
-  scenario 'player 2 status' do
+feature 'At the beginning' do
+  scenario 'player_2 should show hit_points' do
     sign_in_and_play
     expect(page).to have_content 'Tim: 60HP'
   end
+
+  scenario "it should show that it is player_1's turn" do
+    sign_in_and_play
+    expect(page).to have_content "Rosie's turn"
+  end
 end
+
 
 feature 'Attack player' do
   scenario 'attack player 2' do
@@ -25,6 +31,12 @@ feature 'Attack player' do
     sign_in_and_play
     attack
     expect(page).to have_content 'Rosie: 50HP'
+  end
+
+  scenario "it should now be Tim's turn" do
+    sign_in_and_play
+    attack
+    expect(page).to have_content "Tim's turn"
   end
 
   scenario 'reduce player 1 and player 2 HP by 10' do
