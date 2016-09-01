d382b554fd9b322324c10f64eb744fadac0f3bb8
Tim3tang <Tim3tang@gmail.com>
2016-09-01 10:57:50 +0100

    ch21 completed

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -2,20 +2,14 @@ require 'spec_helper'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Rosie'
-    fill_in :player_2_name, with: 'Tim'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Rosie vs. Tim'
   end
 end
 
 feature 'See hit points' do
   scenario 'player 2 status' do
-    visit ('/')
-    fill_in :player_1_name, with: 'Matthew'
-    fill_in :player_2_name, with: 'Tim'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Tim: 60HP'
   end
 end
