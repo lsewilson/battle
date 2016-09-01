67d6aa8b70a682041cbb5ff32affc8e0b52501d4
lsewilson <laurawilson@hotmail.co.uk>
2016-09-01 11:18:04 +0100

    adds HP feature

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,3 +1,13 @@
 require 'spec_helper'
 
-feature 'hit-points'
+feature 'hit-points' do
+
+  scenario "player 1 can see player 2's HP" do
+    visit('/')
+    fill_in :player_1_name, with: 'Laura'
+    fill_in :player_2_name, with: 'Joseph'
+    click_button 'Submit'
+    expect(page).to have_content 'Joseph: 100HP'
+  end
+
+end
