cf905d40bb798f84d29e12c2f26c11189609e283
lsewilson <laurawilson@hotmail.co.uk>
2016-09-01 11:35:22 +0100

    adds attack feauture

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'attack opponent' do
+
+  scenario 'player 1 attacks player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content("You attacked Joseph")
+  end
+
+end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature 'hit-points' do
 
   scenario "player 1 can see player 2's HP" do
-    visit('/')
-    fill_in :player_1_name, with: 'Laura'
-    fill_in :player_2_name, with: 'Joseph'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Joseph: 100HP'
   end
 
